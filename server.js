27d9980b525663c031355058fc7d1e6c92b3744b
Trini Le <trinile@gmail.com>
2017-04-27 15:12:32 -0700

    initialize simple server configs

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import graphQLHTTP from 'express-graphql';
+import path from 'path';
+import webpack from 'webpack';
+import bodyParser from 'body-parser';
+
+import { graphQLConfig } from '../webpack.config.js';
+
+//database and schema
+import { Schema } from './data/schema';
+import mongoose from 'mongoose';
+import config from './config';
+
+const GRAPHQL_PORT = config.port;
+
+let graphQLServer;
+
+const app = express();
+
+app.use(bodyParser.json()) //parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text({ type: 'application/graphql' }));
+
+app.use('/graphql'. graphQLHTTP({
+  graphiql: true,
+  pretty: true,
+  schema: Schema,
+}));
+
+graphQLServer = app.listen(GRAPHQL_PORT, err => {
+  if (err) {
+    return err;
+  }
+  console.log(`GraphQL server running on http://localhost:${PORT}/graphql`)
+})
